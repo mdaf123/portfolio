31d7af9f29dfc21ea29bfe5a1adc90f2d9f617fd
mdaf123 <140780519+mdaf123@users.noreply.github.com>
2024-09-22 21:40:19 -0400

    Update BMI Calculator (Metric System Units).py

--- BMI Calculator (Metric System Units).py
+++ BMI Calculator (Metric System Units).py
@@ -6,6 +6,11 @@ height=input("What is your height in meters?: ")
 height=float(height)
 bmi=weight/(height*height)
 print("Your BMI is: " + str(bmi))
-if bmi<18.5:print("You are a skinny and petite fella.")
-if bmi>18.5 and bmi<25:print("You are the average fella.")
-if bmi>25:print("You are overweight")
+if bmi<18.5:
+  print("You are underweight.")
+if bmi>18.5 and bmi<25:
+  print("You are average.")
+if bmi>25 and bmi<30:
+  print("You are overweight")
+else:
+  print("You are obese")
